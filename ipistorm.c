87d91312f4866918026f76e1c87df6692c73e8a5
Nicholas Piggin <npiggin@gmail.com>
2022-08-09 15:00:47 +1000

    IPI latency

--- ipistorm.c
+++ ipistorm.c
@@ -15,140 +15,138 @@
 #include <linux/kthread.h>
 #include <linux/delay.h>
 
-static long timeout = 30;
+static long timeout = 10;
 module_param(timeout, long, S_IRUGO);
-MODULE_PARM_DESC(timeout, "Timeout in seconds (default = 30)");
+MODULE_PARM_DESC(timeout, "Timeout in seconds (default = 10)");
 
 static bool wait = true;
 module_param(wait, bool, S_IRUGO);
 MODULE_PARM_DESC(wait, "Wait for IPI to finish? (default true)");
 
-static bool single = false;
-module_param(single, bool, S_IRUGO);
-MODULE_PARM_DESC(single, "IPI to a single destination? (default false, IPI to all)");
+static unsigned long source = 0;
+module_param(source, ulong, S_IRUGO);
+MODULE_PARM_DESC(source, "IPI source CPU (default 0)");
 
-static unsigned long offset = 1;
-module_param(offset, ulong, S_IRUGO);
-MODULE_PARM_DESC(offset, "Offset from current CPU for single destination IPI (default 1)");
-
-static unsigned long mask = 0;
-module_param(mask, ulong, S_IRUGO);
-MODULE_PARM_DESC(mask, "Mask for single destination IPI (default no mask)");
+static unsigned long target = 1;
+module_param(target, ulong, S_IRUGO);
+MODULE_PARM_DESC(target, "IPI target CPU (default 1)");
 
 static unsigned long delay = 0;
 module_param(delay, ulong, S_IRUGO);
 MODULE_PARM_DESC(delay, "Delay between calls in us (default 0)");
 
-static char cpulist_str[256];
-module_param_string(cpulist, cpulist_str, sizeof(cpulist_str), 0644);
-MODULE_PARM_DESC(cpulist, "List of CPUs (default all)");
-
-static unsigned int num_cpus;
-static atomic_t running;
+static atomic_t stop_test;
+static atomic_t source_running;
+static atomic_t target_running;
 
 static DECLARE_COMPLETION(ipistorm_done);
 
+static unsigned long delta_hist[32];
+
+static unsigned long remote_tb;
 static void do_nothing_ipi(void *dummy)
 {
+	remote_tb = mftb();
 }
 
-static int ipistorm_thread(void *data)
+static int ipistorm_source_thread(void *data)
 {
-	unsigned long mycpu = (unsigned long)data;
-	unsigned long target_cpu;
-	unsigned long jiffies_start;
+	unsigned long tb_start, tb;
+	unsigned long min = ~0UL, max = 0, total = 0;
+	unsigned long i = 0;
 
-	atomic_inc(&running);
+	atomic_inc(&source_running);
+	smp_mb();
+	while (!atomic_read(&target_running))
+		cpu_relax();
 
-	while (atomic_read(&running) < num_cpus) {
-		if (need_resched())
-			schedule();
-	}
+	tb_start = mftb();
 
-	if (mask) {
-		unsigned long base = mycpu & ~mask;
-		unsigned long off = (mycpu + offset) & mask;
-		target_cpu = (base + off) % num_cpus;
-	} else {
-		target_cpu = (mycpu + offset) % num_cpus;
-	}
+	while ((tb = mftb()) - tb_start < tb_ticks_per_usec * timeout * USEC_PER_SEC) {
+		unsigned long delta; 
 
-	pr_info("%lu -> %lu\n", mycpu, target_cpu);
+		smp_call_function_single(target, do_nothing_ipi, NULL, wait);
 
-	if (single && !cpu_online(target_cpu)) {
-		pr_err("CPU %lu not online\n", target_cpu);
-		return 0;
-	}
+		delta = tb_to_ns(remote_tb - tb);
 
-	jiffies_start = jiffies;
-
-	while (jiffies < (jiffies_start + timeout*HZ)) {
-		if (single)
-			smp_call_function_single(target_cpu,
-						 do_nothing_ipi, NULL, wait);
-		else
-			smp_call_function(do_nothing_ipi, NULL, wait);
+		total += delta;
+		if (delta < min)
+			min = delta;
+		if (delta > max)
+			max = delta;
+		delta_hist[ilog2(delta)]++;
 
 		if (delay)
 			usleep_range(delay, delay+1);
 		else
-			cond_resched();
+//			cond_resched();
+			;
+
+		i++;
 	}
 
-	if (atomic_dec_and_test(&running))
-		complete(&ipistorm_done);
+	atomic_inc(&stop_test);
+
+	printk("%lu IPIs completed\n", i);
+	printk("min=%luns max=%luns avg=%luns\n", min, max, total / i);
+	for (i = 0; i < 32; i++)
+		printk("ns < %lu = %lu\n", 1UL << i, delta_hist[i]);
+
+	complete(&ipistorm_done);
+
+	return 0;
+}
+
+static int ipistorm_target_thread(void *data)
+{
+	atomic_inc(&target_running);
+	smp_mb();
+	while (!atomic_read(&stop_test))
+		;
 
 	return 0;
 }
 
 static int __init ipistorm_init(void)
 {
-	unsigned long cpu;
-	cpumask_var_t mask;
-	int ret = 0;
+	struct task_struct *s, *t;
+
+	pr_info("CPU%lu -> CPU%lu\n", source, target);
 
 	init_completion(&ipistorm_done);
 
-	if (!zalloc_cpumask_var(&mask, GFP_KERNEL))
-		return -ENOMEM;
-
-	if (cpulist_str[0]) {
-		ret = cpulist_parse(cpulist_str, mask);
-		if (ret)
-			goto out_free;
-
-		if (!cpumask_subset(mask, cpu_online_mask)) {
-			pr_err("Invalid CPU list: %s\n", cpulist_str);
-			ret = -EINVAL;
-			goto out_free;
-		}
-	} else {
-		cpumask_copy(mask, cpu_online_mask);
+	t = kthread_create(ipistorm_target_thread, NULL,
+			   "ipistorm_target/%lu", target);
+	if (IS_ERR(t)) {
+		pr_err("kthread_create on CPU %lu failed\n", target);
+		return PTR_ERR(t);
 	}
 
-	num_cpus = cpumask_weight(mask);
+	kthread_bind(t, target);
+	wake_up_process(t);
 
-	for_each_cpu(cpu, mask) {
-		struct task_struct *p;
-		p = kthread_create(ipistorm_thread, (void *)cpu,
-				   "ipistorm/%lu", cpu);
-		if (IS_ERR(p)) {
-			pr_err("kthread_create on CPU %lu failed\n", cpu);
-			atomic_inc(&running);
-		} else {
-			kthread_bind(p, cpu);
-			wake_up_process(p);
-		}
+	s = kthread_create(ipistorm_source_thread, NULL,
+			   "ipistorm/%lu", source);
+	if (IS_ERR(s)) {
+		pr_err("kthread_create on CPU %lu failed\n", source);
+		atomic_inc(&stop_test);
+		kthread_stop(t);
+		return PTR_ERR(s);
 	}
 
-out_free:
-	free_cpumask_var(mask);
-	return ret;
+	kthread_bind(s, source);
+	wake_up_process(s);
+
+	wait_for_completion(&ipistorm_done);
+
+	kthread_stop(s);
+	kthread_stop(t);
+
+	return -EAGAIN;
 }
 
 static void __exit ipistorm_exit(void)
 {
-	wait_for_completion(&ipistorm_done);
 }
 
 module_init(ipistorm_init)
